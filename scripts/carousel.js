2517b9e77fef156943a4f01b85f64bd3cf58224d
Lauren <lauren.c4231@gmail.com>
2020-05-18 17:51:21 +0100

    Add swipe events

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -94,3 +94,50 @@ const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
         nextButton.classList.remove('hidden');
     }
 };
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(event) {
+    return event.touches || event.originalEvent.touches;
+}
+
+function handleTouchStart(event) {
+    const firstTouch = getTouches(event)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(event) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = event.touches[0].clientX;
+    var yUp = event.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            const currentSlide = track.querySelector('.slide--current');
+            const nextSlide = currentSlide.nextElementSibling;
+            const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+            moveToSlide(track, currentSlide, nextSlide);
+            hideShowArrows(slides, prevButton, nextButton, nextIndex);
+        } else {
+            const currentSlide = track.querySelector('.slide--current');
+            const prevSlide = currentSlide.previousElementSibling;
+            const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+            moveToSlide(track, currentSlide, prevSlide);
+            hideShowArrows(slides, prevButton, nextButton, prevIndex);
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+}
