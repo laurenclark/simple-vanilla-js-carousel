15c13ed61c3bce837191ed74677d8f00a87b1b61
Lauren <lauren.c4231@gmail.com>
2020-05-18 01:06:47 +0100

    Add button click handlers

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -4,3 +4,32 @@ const slides = Array.from(track.children);
 // Controls
 const nextButton = document.querySelector('.carousel__button.right');
 const prevButton = document.querySelector('.carousel__button.left');
+
+// Slide positioning
+const setSlidePosition = (slide, i) => {
+    return (slide.style.left =
+        slide.childNodes[1].getBoundingClientRect().width * i + 'px');
+};
+slides.forEach(setSlidePosition);
+
+// Slide Movement
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('slide--current');
+    targetSlide.classList.add('slide--current');
+};
+
+// Button Handlers
+nextButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.slide--current');
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+    moveToSlide(track, currentSlide, nextSlide);
+});
+
+prevButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.slide--current');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+    moveToSlide(track, currentSlide, prevSlide);
+});
